29338327517e4ba00c6aae077fc6dfe75501c4b0
brandonmarlowe <b.marlowe@vikes.csuohio.edu>
2018-05-17 09:42:20 -0400

    moved the function calls inside main method

--- face_recog.py
+++ face_recog.py
@@ -6,11 +6,12 @@ import numpy as np
 subject = ["", "subject2", "subject1"]
 saved_model_path = 'saved_model/smart_doorbell_model.yaml'
 test_data_path = 'test_data/'
+face_recognizer = cv2.face.createLBPHFaceRecognizer()
 
 def detect_face(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     face_cascade = cv2.CascadeClassifier('opencv_files/lbpcascade_frontalface.xml')
-    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.2, minNeighbors=5);
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.2, minNeighbors=5)
     
     if (len(faces) == 0):
         return None, None
@@ -20,21 +21,21 @@ def detect_face(image):
     return gray_image[y:y+w, x:x+h], faces[0]
 
 
-def prepare_training_data(data_folder_path):
+def prepare_data(data_folder_path):
     
     dirs = os.listdir(data_folder_path)
     
     faces = []
     labels = []
 
-    print "Training model..."
+    print "Data preparation phase started\n"
 
     for dir_name in dirs:
         
         if not dir_name.startswith("s"):
             continue;
 
-        label = int(dir_name.replace("s", ""))
+        label = int(dir_name.replace("subject_", ""))
         subject_dir_path = data_folder_path + "/" + dir_name
         subject_images_names = os.listdir(subject_dir_path)
 
@@ -44,9 +45,9 @@ def prepare_training_data(data_folder_path):
                 continue;
 
             image_path = subject_dir_path + "/" + image_name
-            image = cv2.imread(image_path)
+            image = cv2.imread(image_path) 
             
-            cv2.imshow("training_data", cv2.resize(image, (400, 500)))
+            cv2.imshow(subject_dir_path, cv2.resize(image, (400, 500)))
             cv2.waitKey(100)
 
             face, rect = detect_face(image)
@@ -54,22 +55,14 @@ def prepare_training_data(data_folder_path):
             if face is not None:
                 faces.append(face)
                 labels.append(label)
-            
+
+    print "Data preparation phase complete\n"
+
     cv2.destroyAllWindows()
     cv2.waitKey(1)
     cv2.destroyAllWindows()
     
     return faces, labels
-
-print("Preparing data...\n")
-faces, labels = prepare_training_data("training_data")
-print("\nData prepared\n")
-
-face_recognizer = cv2.face.createLBPHFaceRecognizer()
-face_recognizer.train(faces, np.array(labels))
-face_recognizer.save(saved_model_path)
-# to load the model
-# face_recognizer.load(saved_model_path)
 
 def draw_rectangle(image, rect):
     (x, y, w, h) = rect
@@ -79,6 +72,8 @@ def draw_text(image, text, x, y):
     cv2.putText(image, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 
 def predict(test_image, img):
+    global face_recognizer
+
     image = test_image.copy()
     face, rect = detect_face(image)
     label, confidence = face_recognizer.predict(face)
@@ -90,13 +85,35 @@ def predict(test_image, img):
     
     return image
 
-print("Predicting images...\n")
 
-test_dir = os.listdir(test_data_path)
-count = 1
+def main():
+    
+    faces, labels = prepare_data("training_data")
+    
+    face_recognizer.train(faces, np.array(labels))
+    print "Model training complete\n"
 
-for img in test_dir:
-    cv2.imshow("Subject {}".format(count), cv2.resize(predict(cv2.imread("test_data/" + img), str(img)), (400, 500)))
-    count += 1
+    face_recognizer.save(saved_model_path)
+    print "Model saved as: " + saved_model_path + "\n"
+    
+    # to load the model use the code below
+    # face_recognizer.load(saved_model_path)
 
-cv2.waitKey(0)
+    print "Subject prediction phase started\n"
+
+    test_dir = os.listdir(test_data_path)
+    
+    count = 1
+
+    for img in test_dir:
+        cv2.imshow("Subject {}".format(count), cv2.resize(predict(cv2.imread("test_data/" + img), str(img)), (400, 500)))
+        count += 1
+
+    print "\nSubject predicition phase complete"
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    main()
+    
+
